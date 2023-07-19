6a2ec4e085b45fa09c1c1bb4405ab79e0f015db0
ibar <ibar.student@simad.edu.so>
2023-07-19 19:59:52 +0300

    initial project setup

---
+++ epasswd/settings/Production.py
@@ -0,0 +1,5 @@
+from .Base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
---
+++ epasswd_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EpasswdAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "epasswd_app"
