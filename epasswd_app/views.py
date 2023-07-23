24fa14d2ed8a44e6cc503a2598e636ab7013015c
ibar <ibar.student@simad.edu.so>
2023-07-23 20:53:41 +0300

    Created basic templates and views for the app created a navbar that is responsive an urls for all the pages

--- epasswd_app/views.py
+++ epasswd_app/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 
 def home(request):
     return render(request, 'pages/home.html')
+
+
+def generator(request):
+    # generating staff
+    return HttpResponse('under construction')
+
+
+def passwordchecking(request):
+    # password strength checker
+    return HttpResponse('.....')
+
+
+def SavePassword(request):
+    # saving passwords
+    return HttpResponse('under construction')
+
+
+def dashboard(request):
+    # dashboard
+    return HttpResponse('Dashboard loading.....')
+
+
+def login(request):
+    # login existing users
+    return HttpResponse('loged')
+
+
+def signup(request):
+    # create new users
+    return HttpResponse('created')
