26e2ffc796623145beb41f3f28b42059b1a2456e
ibar <ibar.student@simad.edu.so>
2023-07-25 13:41:10 +0300

    Created models for the passwords we first created two models for the passwords that the password would be splited into them and those models wouldn't be saved in the same database

--- epasswd_app/models.py
+++ epasswd_app/models.py
@@ -6,3 +6,33 @@ class User(AbstractUser):
 
     def __str__(self) -> str:
         return self.username
+
+
+class passwords(models.Model):
+    """
+    The encrypted password would split between this model and the partialpass
+    model but this model would contain the most amount of the informations of the
+    passwords.
+    The owner field is integerfield and not foreign because the owner and passwords model
+    wouldn't be in the same database
+    """
+
+    owner = models.IntegerField()
+    name = models.CharField(max_length=55, unique=True)
+    passwd = models.BinaryField()
+    tag = models.BinaryField()
+    nonce = models.BinaryField()
+    created_at = models.DateField(auto_now=True)
+
+
+class partialpass(models.Model):
+    """
+    This model saves the remaining half of the encrypted password.
+    The reason why we use the Integerfield and not the foreign key 
+    is the models wouldn't be saved in the same database so to connected them
+    we faced some issue there could be some tweeks but i think this is the best 
+    and easiest possible
+    """
+    partof = models.IntegerField()
+    partial = models.BinaryField()
+    owner = models.IntegerField(default=None)
